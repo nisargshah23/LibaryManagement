44c72199d116267bb4034ba08b3f4e3b930661a5
nisarg <shahnisarg23@gmail.com>
2025-03-19 17:25:42 +0530

    changes code

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,9 @@ const bookRoutes = require("./routes/bookRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["https://libary-management-wjnh.vercel.app/"],credential:true}
+));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
