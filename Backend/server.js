68f1274fe983b3bc7836672138a1ecb036d47986
Nisarg Shah <43407970+nisargshah23@users.noreply.github.com>
2025-03-21 22:26:06 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const bookRoutes = require("./routes/bookRoutes");
 const app = express();
 
 
-const allowedOrigins = [process.env.FRONTEND_URL || "https://superlative-kelpie-f3d91c.netlify.app"];
+const allowedOrigins = [process.env.FRONTEND_URL || "https://velvety-sopapillas-159c9a.netlify.app/"];
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
