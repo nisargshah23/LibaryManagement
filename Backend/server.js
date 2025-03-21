a6d1f01a57d5d393a415e2e5d14cb513e858254f
Nisarg Shah <43407970+nisargshah23@users.noreply.github.com>
2025-03-21 21:12:40 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ["https://superlative-kelpie-f3d91c.netlify.app"], 
+    origin: ["https://superlative-kelpie-f3d91c.netlify.app","http://localhost:5173"], 
     credentials: true,  
     methods: ["GET", "POST", "PUT", "DELETE"],  
     allowedHeaders: ["Content-Type", "Authorization"] 
